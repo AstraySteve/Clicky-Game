c8ccfb553f2c1a923533a21e61b516d68404e6d6
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-09-12 14:44:32 -0400

    adding elements

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 
 class Game extends Component{
-    render(){
-        return(
-            <div>
-                <div className="jumbotron">
-                    <h1>Clicky Game!</h1>
-                    <h2>Click on an image to earn points, but don't click any more than once</h2>
-                </div>   
-            </div> 
-        )
-    }
+  render(){
+    return(
+      <div>
+        <div className="jumbotron">
+          <h1>Clicky Game!</h1>
+          <h2>Click on an image to earn points, but don't click any more than once</h2>
+        </div>   
+      </div> 
+    )
+  }
 }
 
 export default Game;
